749031aac4cb6d22d05647abca85887855928df6
swass <dasswastika@gmail.com>
2023-04-07 08:49:55 +0530

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,1 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoggedinService } from '../loggedin.service';
 
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  loggedInUser = localStorage.getItem('user');
+  loggedin: boolean = false;
+  constructor(private route:Router, private _onlogin: LoggedinService) { }
+
+  ngOnInit(): void {
+    console.log("inside home menu"+this. _onlogin.loggedIn);
+    this._onlogin.login$
+      .subscribe(
+        message => {
+          this.loggedin = message;
+          console.log("inside subscribe "+this.loggedin)
+        }
+      );   
+  }
+  
+
+  
+  onLogout() {
+    localStorage.removeItem('user');
+    this.loggedInUser = null;
+    this.loggedin = false;
+    this.route.navigate(['/','login'])
+  }
+}
